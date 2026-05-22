0602ea996771ca8f5dacbb048f6bc598ce0ed167
izabelem167-boop <izabelem167@gmail.com>
2026-05-22 16:23:14 +0100

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <div style={{
+      background: "#111",
+      color: "white",
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      fontSize: "40px"
+    }}>
+      MusicAI funcionando 🎵
+    </div>
+  )
+}
