a706358d88116b31a55fffddfc5c4d9b59adbba5
izabelem167-boop <izabelem167@gmail.com>
2026-05-26 13:19:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 import React from 'react'
 
 export default function App() {
-  return <h1 style={{color:'white',background:'black',height:'100vh',display:'flex',alignItems:'center',justifyContent:'center',margin:0}}>FUNCIONOU!</h1>
+  return (
+    <h1 style={{
+      color: 'white',
+      background: 'black',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      margin: 0,
+      fontFamily: 'sans-serif'
+    }}>
+      FUNCIONOU!
+    </h1>
+  )
 }
