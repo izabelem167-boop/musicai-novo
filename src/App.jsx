61fcf94f2645df6ded234e2d4a1577836568ee1b
izabelem167-boop <izabelem167@gmail.com>
2026-05-28 12:44:12 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY
-)
+  import.meta.env.VITE_SUPABASE_KEY
 
 export default function App() {
   const [email, setEmail] = useState("");
