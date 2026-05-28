8890896b6b50627c768a68c72b4db6535bbab890
izabelem167-boop <izabelem167@gmail.com>
2026-05-28 12:59:32 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
+const supabase = createClient);
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_KEY
 
